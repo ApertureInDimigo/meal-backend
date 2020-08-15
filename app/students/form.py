dc54a8f5d769ffdf8bb5803956726e52368e76a8
woduq1414 <jjy37777@naver.com>
2020-08-15 21:18:56 +0900

    폼에 data_key 설정

--- app/students/form.py
+++ app/students/form.py
@@ -7,10 +7,10 @@ class StudentSchema(Schema):
     id = fields.String(required=True, validate=validate.Regexp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,15}$'))
     password = fields.String(required=True, validate=validate.Regexp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&-_])[A-Za-z\d$@$!%*#?&-_]{6,15}$'))
     nickname = fields.String(required=True, validate=validate.Regexp(r'^[가-힣0-9A-Za-z]{2,10}$'))
-    school_grade = fields.Integer(required=True)
-    school_class = fields.Integer(required=True)
-    school_id = fields.Integer(required=True)
-    school_code = fields.String(required=False)
+    school_grade = fields.Integer(required=True, data_key="schoolGrade")
+    school_class = fields.Integer(required=True, data_key="schoolClass")
+    school_id = fields.Integer(required=True, data_key="schoolId")
+    school_code = fields.String(required=False, data_key="schoolCode")
 
 
 
