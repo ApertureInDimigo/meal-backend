94d5e367321b581b8b66bd01cad02db8833365f7
woduq1414 <jjy37777@naver.com>
2020-11-14 17:19:38 +0900

    과거 결과도 볼 수 있게

--- app/meals/api.py
+++ app/meals/api.py
@@ -190,18 +190,25 @@ class _RatingStar(Resource):
         student, school = get_identify() or (None, None)
         if student is None: return {"message": "올바르지 않은 회원 정보입니다."}, 401
 
-        old_rating_row = MenuRating.query.filter_by(school=school, student=student,
-                                                    menu_date=str_to_date(args["menu_date"])) \
-            .filter(MenuRating.star.isnot(None)).all()
+        if is_same_date(datetime.now(), str_to_date(args["menu_date"])):
 
-        # if old_rating_row is None:
-        #     return {"message": "평가한 후에 별점을 볼 수 있습니다."}, 409
 
-        old_rating_menu_seq_list = [rating_row.menu_seq for rating_row in old_rating_row]
+            old_rating_row = MenuRating.query.filter_by(school=school, student=student,
+                                                        menu_date=str_to_date(args["menu_date"])) \
+                .filter(MenuRating.star.isnot(None)).all()
 
-        rating_rows = MenuRating.query.filter_by(school=school, menu_date=str_to_date(args["menu_date"]),
-                                                 banned=False).filter(MenuRating.star.isnot(None)).filter(
-            MenuRating.menu_seq.in_(old_rating_menu_seq_list)).all()
+            # if old_rating_row is None:
+            #     return {"message": "평가한 후에 별점을 볼 수 있습니다."}, 409
+
+            old_rating_menu_seq_list = [rating_row.menu_seq for rating_row in old_rating_row]
+
+            rating_rows = MenuRating.query.filter_by(school=school, menu_date=str_to_date(args["menu_date"]),
+                                                     banned=False).filter(MenuRating.star.isnot(None)).filter(
+                MenuRating.menu_seq.in_(old_rating_menu_seq_list)).all()
+
+        else:
+            rating_rows = MenuRating.query.filter_by(school=school, menu_date=str_to_date(args["menu_date"]),
+                                                     banned=False).filter(MenuRating.star.isnot(None)).all()
 
         rating_data = defaultdict(list)
 
@@ -236,8 +243,15 @@ class _RatingStar(Resource):
             print(e.messages)
             return {"message": "파라미터 값이 유효하지 않습니다."}, 400
         print(args)
+
+        if is_same_date(str_to_date(args["menu_date"]), datetime.now()):
+            return {"message" : "당일에만 평가할 수 있습니다."}, 406
+
+
         student, school = get_identify() or (None, None)
         if student is None: return {"message": "올바르지 않은 회원 정보입니다."}, 401
+
+
 
         lunch_meal_data = get_day_meal(school, args["menu_date"])
 
@@ -336,12 +350,6 @@ class _RatingQuestion(Resource):
         }
 
 
-
-
-
-
-
-
 class _RatingAnswer(Resource):
     @return_500_if_errors
     @login_required
@@ -358,13 +366,14 @@ class _RatingAnswer(Resource):
 
         student, school = get_identify() or (None, None)
         if student is None: return {"message": "올바르지 않은 회원 정보입니다."}, 401
+        
+        if is_same_date(datetime.now(), str_to_date(args["menu_date"])):
+            old_rating_row = MenuRating.query.filter_by(school=school, student=student,
+                                                        menu_date=str_to_date(args["menu_date"]), menu_seq=args["menu_seq"]) \
+                .filter(MenuRating.questions.isnot(None)).first()
 
-        old_rating_row = MenuRating.query.filter_by(school=school, student=student,
-                                                    menu_date=str_to_date(args["menu_date"]), menu_seq=args["menu_seq"]) \
-            .filter(MenuRating.questions.isnot(None)).first()
-
-        if old_rating_row is None:
-            return {"message": "평가한 후에 응답 결과를 볼 수 있습니다."}, 409
+            if old_rating_row is None:
+                return {"message": "평가한 후에 응답 결과를 볼 수 있습니다."}, 409
 
         from sqlalchemy.orm import load_only
         rating_rows = MenuRating.query.options(
@@ -408,6 +417,10 @@ class _RatingAnswer(Resource):
         except marshmallow.exceptions.ValidationError as e:
             print(e.messages)
             return {"message": "파라미터 값이 유효하지 않습니다."}, 400
+
+
+        if is_same_date(str_to_date(args["menu_date"]), datetime.now()):
+            return {"message" : "당일에만 평가할 수 있습니다."}, 406
 
         student, school = get_identify() or (None, None)
         if student is None: return {"message": "올바르지 않은 회원 정보입니다."}, 401
@@ -614,8 +627,6 @@ class _RatingFavoriteAll(Resource):
 
         student_id = g.user_id
         args = request.args
-
-
 
         student, school = get_identify() or (None, None)
         if student is None: return {"message": "올바르지 않은 회원 정보입니다."}, 401
