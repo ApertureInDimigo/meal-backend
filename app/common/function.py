5bf8bdd0b73a78106b1d563fe153949d2c1458f1
woduq1414 <jjy37777@naver.com>
2020-11-14 19:35:20 +0900

    Update function.py

--- app/common/function.py
+++ app/common/function.py
@@ -208,7 +208,7 @@ def dict_mean(dict_list):
 
     mean_dict = {}
     for key in dict_list[0].keys():
-        mean_dict[key] = sum(d[key] for d in dict_list) / len(dict_list)
+        mean_dict[key] = sum([d[key] for d in dict_list if key in d]) / len([d[key] for d in dict_list if key in d])
     return mean_dict
 
 
